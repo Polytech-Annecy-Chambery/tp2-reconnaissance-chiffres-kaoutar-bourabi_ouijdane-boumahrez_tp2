edbbf7d4bb68b01d6c8404ba93920a5ecc7ea65f
Kaoutar-Bourabi <72504063+Kaoutar-Bourabi@users.noreply.github.com>
2021-11-19 19:16:16 +0100

    Add files via upload

--- src/reconnaissance.py
+++ src/reconnaissance.py
@@ -12,5 +12,24 @@ def lecture_modeles(chemin_dossier):
 
 
 def reconnaissance_chiffre(image, liste_modeles, S):
-    pass
+    bin_im = image.binarisation(S)
+    loc_im = bin_im.localisation()
+    
+    #modele_0  = liste_modeles[0]
+    #W  = modele_0.W
+    #H = modele_0.H
+    #res = loc_im.resize(H,W)
+    
+    prop = 0 
+    #i_prop = 0 
+    
+    for i in range(len(liste_modeles)):
+        res=loc_im.resize(liste_modeles[i].H,liste_modeles[i].W)
+        if res.similitude(liste_modeles[i]) > prop :
+            prop = res.similitude(liste_modeles[i])
+            i_prop = i
+    return i_prop        
+            
+            
+    
 
